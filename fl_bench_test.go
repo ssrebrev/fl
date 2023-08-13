a1e1b3a1a3749a293b31e42bc246c6d1f9744095
ssrebrev <ssrebrev@vmware.com>
2023-08-13 17:44:51 +0300

    Add some benchmarks

--- fl_bench_test.go
+++ fl_bench_test.go
@@ -1,6 +1,15 @@
 package fl
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
+
+const (
+	bufSizeAlloc = 128
+	minRandBuf   = 66
+	randBuf      = 1024
+)
 
 func BenchmarkFreeList(b *testing.B) {
 	fl := FreeList[byte]{}
@@ -18,10 +27,18 @@ func BenchmarkMpFreeList(b *testing.B) {
 	}
 }
 
+func BenchmarkMemAlloc(b *testing.B) {
+	bufAlloc := bufSizeAlloc
+	for i := 0; i < b.N; i++ {
+		v := make([]byte, bufAlloc)
+		_ = v
+	}
+}
+
 func BenchmarkSliceFreeList(b *testing.B) {
 	fl := SliceFreeList[byte]{}
 	for i := 0; i < b.N; i++ {
-		v := fl.GetOrCreate(10)
+		v := fl.GetOrCreate(bufSizeAlloc)
 		fl.Put(v)
 	}
 }
@@ -29,7 +46,33 @@ func BenchmarkSliceFreeList(b *testing.B) {
 func BenchmarkSliceMpFreeList(b *testing.B) {
 	fl := SliceMpFreeList[byte]{}
 	for i := 0; i < b.N; i++ {
-		v := fl.GetOrCreate(10)
+		v := fl.GetOrCreate(bufSizeAlloc)
+		fl.Put(v)
+	}
+}
+
+func BenchmarkRandMemAlloc(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		bufAlloc := minRandBuf + rand.Intn(randBuf)
+		v := make([]byte, bufAlloc)
+		_ = v
+	}
+}
+
+func BenchmarkRandSliceFreeList(b *testing.B) {
+	fl := SliceFreeList[byte]{}
+	for i := 0; i < b.N; i++ {
+		bufAlloc := minRandBuf + rand.Intn(randBuf)
+		v := fl.GetOrCreate(bufAlloc)
+		fl.Put(v)
+	}
+}
+
+func BenchmarkRandSliceMpFreeList(b *testing.B) {
+	fl := SliceMpFreeList[byte]{}
+	for i := 0; i < b.N; i++ {
+		bufAlloc := minRandBuf + rand.Intn(randBuf)
+		v := fl.GetOrCreate(bufAlloc)
 		fl.Put(v)
 	}
 }
